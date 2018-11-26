3bd37cca459fde4734f0d9cedbee373e67ea23c7
Dan Roder <dsroder@gmail.com>
2018-11-26 17:29:41 -0500

    string formatting

--- helloworld/HelloWorld.py
+++ helloworld/HelloWorld.py
@@ -1,6 +1,4 @@
 __author__ = 'dev'
-print('Hello World')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The End")
+greeting = 'Hello'
+name = input("Please enter your name: ")
+print(greeting + ' ' + name)
